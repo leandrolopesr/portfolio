b2e00ac391d177f54bdfce3ba4b71a4dea01b758
LeandroLopes <rafalopes580@gmail.com>
2022-10-29 23:05:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+function typeWriter (el){
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i)  => {
+        setTimeout(() => (el.innerHTML += letter), 140 * i);
+    });
+}
+
+typeWriter(myname);
